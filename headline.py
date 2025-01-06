227e4bfb2d2a2ec8dbf8015715d30f855624361d
divyanshu-912 <divyanshukannau143@outlook.com>
2025-01-06 20:02:21 +0530

    Update headline.py

--- headline.py
+++ headline.py
@@ -1,5 +1,5 @@
 import pandas as pd
- import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 from torch.utils.data import DataLoader, Dataset
